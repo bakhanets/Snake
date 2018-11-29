afa01cf2b57afaf926e20aa3872372bcfd68c5c9
bakhanets <32462637+bakhanets@users.noreply.github.com>
2018-11-29 19:25:30 +0700

    Add files via upload

---
+++ Snake/src/MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow()
+    {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(340,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mv=new MainWindow();
+    }
+}
